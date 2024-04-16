597f18a71553b599b5881359e432e5f74ed9a586
Gabriela França <gabifmouraroc@outlook.com>
2024-04-16 20:11:58 -0300

    correção do texto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,20 @@
-export default async function Home() {
-  await new Promise((resolve) => setTimeout(resolve, 2000))
-  const response = await fetch('https://api.github.com/users/gabifrancamr')
-  const user = await response.json()
-  return <pre>{JSON.stringify(user, null, 2)}</pre>
+import { Suspense } from 'react'
+
+import GithubProfile from '@/components/github-profile'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi adipisci
+        modi beatae laboriosam deserunt voluptas repellat quo, eaque quis at,
+        repudiandae aliquid nemo corporis tenetur possimus, ducimus sit
+        molestiae! Culpa!
+      </p>
+      <Suspense fallback={<p>Carregando</p>}>
+        <GithubProfile />
+      </Suspense>
+    </div>
+  )
 }
