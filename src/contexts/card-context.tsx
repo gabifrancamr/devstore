f89192709126f804375beac27a367bca7c166aef
Gabriela França <gabifmouraroc@outlook.com>
2024-04-23 07:37:58 -0300

    contexto do carrinho

---
+++ src/contexts/card-context.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import { ReactNode, createContext, useContext, useState } from 'react'
+
+interface CartItem {
+  productId: string
+  quantity: number
+}
+
+interface CartContextTypes {
+  items: CartItem[]
+  addToCart: (productId: string) => void
+}
+
+const CartContext = createContext({} as CartContextTypes)
+
+interface CartProviderProps {
+  children: ReactNode
+}
+
+export function CartProvider({ children }: CartProviderProps) {
+  const [cartItems, setCartItems] = useState<CartItem[]>([])
+
+  function addToCart(productId: string) {
+    setCartItems((state) => {
+      const productInCart = state.some((item) => item.productId === productId)
+
+      if (productInCart) {
+        return state.map((item) => {
+          if (item.productId === productId) {
+            return { ...item, quantity: item.quantity + 1 }
+          } else {
+            return item
+          }
+        })
+      } else {
+        return [...state, { productId, quantity: 1 }]
+      }
+    })
+  }
+
+  return (
+    <CartContext.Provider value={{ items: cartItems, addToCart }}>
+      {children}
+    </CartContext.Provider>
+  )
+}
+export const useCart = () => useContext(CartContext)
